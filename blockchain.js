98a9a8c2ae4d7a0f62d21fd3166606be63c8042b
fatemeh-askari <145195116+fatemeh-askari@users.noreply.github.com>
2023-12-06 00:29:33 +0330

    chain validation codes

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,5 @@
 const Block = require('./block');
-const BLock = require('./block')
+const cryptoHash = require('./crypto-hash');
 
 class Blockchain{
   constructor(){
@@ -12,6 +12,30 @@ class Blockchain{
     });
 
     this.chain.push(newBlock);
+  }
+
+  static isValidChain(chain){
+    if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())){
+      return false;
+    }
+
+    for(let i=1; i<chain.length; i++){
+      const block = chain[i];
+      const actualLastHash = chain[i-1].hash;
+
+      const timestamp = block.timestamp;
+      const lastHash = block.lastHash;
+      const hash = block.hash;
+      const data = block.data;
+
+      // const {timestamp, lastHash, hash, data} = block;
+
+      if(lastHash !== actualLastHash) return false;
+      if(hash !== cryptoHash(timestamp, lastHash, data)) return false;
+    }
+
+    return true;
+
   }
 }
 
