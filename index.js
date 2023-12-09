f502cd3dfe883593dd3375e440403a6b923816eb
fatemeh-askari <145195116+fatemeh-askari@users.noreply.github.com>
2023-12-09 19:15:21 +0330

    post method api

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const app = express();
 
 
 const blockchain = new Blockchain();
+app.use(express.json());
 
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+  const data = req.body.data;
+  // const {data} = re.body;
+
+  blockchain.addBlock({data});
+  res.redirect('/api/blocks');
 });
 
 const PORT = 3000;
