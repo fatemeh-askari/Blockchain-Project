cd11fb12849a0d161c99dfd0f5412e60eaef4d5f
fatemeh-askari <145195116+fatemeh-askari@users.noreply.github.com>
2023-12-09 22:47:36 +0330

    redis and implement pubsub

---
+++ pubsub.js
@@ -0,0 +1,34 @@
+const redis = require('redis');
+
+const CHANNELS = {
+  TEST : 'TEST',
+}
+
+class PubSub {
+  constructor(){
+    this.publisher = redis.createClient();
+    this.subscriber = redis.createClient();
+
+
+    this.subscriber.subscribe(CHANNELS.TEST);
+
+    this.subscriber.on("message", (channel, message) => {
+      this.handleMessage(channel, message);
+    })
+  }
+
+  handleMessage(channel, message){
+    console.log(`Message Resived. channel: ${channel} and message: ${message} .`)
+  }
+}
+
+
+const testPubSub = new PubSub();
+
+setTimeout(() => {
+  testPubSub.publisher.publish(CHANNELS.TEST, 'foo');
+}, 1000)
+
+
+
+
